ab0cef4031a438e43e984133b90eb2f66bd41ca7
Freek Van der Herten <freek@spatie.be>
2024-06-12 14:46:48 +0200

    add DiscoverSolutionProviders

---
+++ tests/DiscoverSolutionProvidersTest.php
@@ -0,0 +1,20 @@
+<?php
+
+use Spatie\ErrorSolutions\Contracts\HasSolutionsForThrowable;
+use Spatie\ErrorSolutions\DiscoverSolutionProviders;
+
+it('can get all solution providers', function() {
+    $providers = DiscoverSolutionProviders::for(['php', 'laravel']);
+
+    expect($providers)
+        ->not()->toBeEmpty()
+        ->toImplement(HasSolutionsForThrowable::class);
+});
+
+it('will discover more solution providers for more types', function() {
+    $phpProviders = DiscoverSolutionProviders::for(['php']);
+
+    $laravelProviders = DiscoverSolutionProviders::for(['php', 'laravel']);
+
+    expect(count($laravelProviders))->toBeGreaterThan(count($phpProviders));
+});
