6fd5734259e6b5ae9e022b13bd88a1c0ea006bb8
Philip Theobald <fylzero@gmail.com>
2026-03-05 08:24:28 -0600

    Add support for bun.lock and bun.lockb files to v1

--- src/SolutionProviders/Laravel/MissingViteManifestSolutionProvider.php
+++ src/SolutionProviders/Laravel/MissingViteManifestSolutionProvider.php
@@ -37,6 +37,8 @@ class MissingViteManifestSolutionProvider implements HasSolutionsForThrowable
     {
         /** @var string */
         $baseCommand = collect([
+            'bun.lockb' => 'bun',
+            'bun.lock' => 'bun',
             'pnpm-lock.yaml' => 'pnpm',
             'yarn.lock' => 'yarn',
         ])->first(fn ($_, $lockfile) => file_exists(base_path($lockfile)), 'npm run');
